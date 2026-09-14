bb6f5154580f480095f4be908d1bc822854bf838
EvelynCPFreitas <evelyn.pacheco@mercadolivre.com>
2026-09-14 18:50:58 -0300

    Change Firebase imports to CDN and update comments
    Updated Firebase imports to use CDN links and added comments in Portuguese.

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Importa usando os links diretos da internet (CDN)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Sua configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCUIIN9NLD3TcSJ78r22UbkMFVGs3D7ryE",
   authDomain: "expediemfluxo.firebaseapp.com",
@@ -16,8 +13,9 @@ const firebaseConfig = {
   measurementId: "G-18WKK75YLX"
 };
 
-// Initialize Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+// Exporta para usar nas outras telas
 export { app, analytics };
